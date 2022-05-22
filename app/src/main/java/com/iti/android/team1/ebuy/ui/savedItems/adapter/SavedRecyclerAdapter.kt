9b7b28222065ddde6824166cd355ddf00ecd85d5
Abdulrahman.ali <Abdulrahman.ali19@outlook.com>
2022-05-22 20:25:42 +0200

    init all saved items screens

---
+++ app/src/main/java/com/iti/android/team1/ebuy/ui/savedItems/adapter/SavedRecyclerAdapter.kt
@@ -0,0 +1,22 @@
+package com.iti.android.team1.ebuy.ui.savedItems.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.iti.android.team1.ebuy.databinding.SavedItemsLayoutBinding
+
+class SavedRecyclerAdapter : RecyclerView.Adapter<SavedRecyclerAdapter.SavedItemsViewHolder>() {
+
+    inner class SavedItemsViewHolder(binding: SavedItemsLayoutBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SavedItemsViewHolder = SavedItemsViewHolder(SavedItemsLayoutBinding.inflate(
+        LayoutInflater.from(parent.context)))
+
+    override fun onBindViewHolder(holder: SavedItemsViewHolder, position: Int) = bindView()
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    private fun bindView () {}
+}
---
+++ app/src/main/java/com/iti/android/team1/ebuy/ui/savedItems/viewmodel/SavedItemsViewModel.kt
@@ -0,0 +1,7 @@
+package com.iti.android.team1.ebuy.ui.savedItems.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class SavedItemsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
