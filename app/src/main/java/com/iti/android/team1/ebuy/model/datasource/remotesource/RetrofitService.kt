e12477bf927b336f74d3533c5ca2a317ef31f5cb
AhmedMohamedAbdallah <57001378+AhmedAbdallah125@users.noreply.github.com>
2022-05-24 00:28:00 +0200

    make brand data request in network layer and repository

--- app/src/main/java/com/iti/android/team1/ebuy/model/datasource/remotesource/RetrofitService.kt
+++ app/src/main/java/com/iti/android/team1/ebuy/model/datasource/remotesource/RetrofitService.kt
@@ -1,5 +1,11 @@
 package com.iti.android.team1.ebuy.model.datasource.remotesource
 
-interface RetrofitService {
+import com.iti.android.team1.ebuy.model.networkresponse.NetworkResponse
+import com.iti.android.team1.ebuy.model.pojo.Brands
+import retrofit2.Response
+import retrofit2.http.GET
 
+interface RetrofitService {
+    @GET("smart_collections.json")
+    suspend fun getAllBrands() :Response<Brands>
 }
--- app/src/main/java/com/iti/android/team1/ebuy/model/datasource/repository/IRepository.kt
+++ app/src/main/java/com/iti/android/team1/ebuy/model/datasource/repository/IRepository.kt
@@ -1,4 +1,9 @@
 package com.iti.android.team1.ebuy.model.datasource.repository
 
+import com.iti.android.team1.ebuy.model.networkresponse.NetworkResponse
+import com.iti.android.team1.ebuy.model.pojo.Brands
+
 interface IRepository {
+    suspend fun getAllBrands() : NetworkResponse<Brands?>
+
 }
