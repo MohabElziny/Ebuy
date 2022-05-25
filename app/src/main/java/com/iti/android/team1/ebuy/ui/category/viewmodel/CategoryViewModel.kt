b010a87ef926deb2aae7576c89db40cd1d2abf49
AhmedMohamedAbdallah <57001378+AhmedAbdallah125@users.noreply.github.com>
2022-05-25 16:46:29 +0200

    edit live data to state flow as response

--- app/src/main/java/com/iti/android/team1/ebuy/ui/category/viewmodel/CategoryViewModel.kt
+++ app/src/main/java/com/iti/android/team1/ebuy/ui/category/viewmodel/CategoryViewModel.kt
@@ -1,7 +1,5 @@
 package com.iti.android.team1.ebuy.ui.category.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.iti.android.team1.ebuy.model.datasource.repository.IRepository
@@ -9,33 +7,35 @@ import com.iti.android.team1.ebuy.model.networkresponse.NetworkResponse
 import com.iti.android.team1.ebuy.model.networkresponse.ResultState
 import com.iti.android.team1.ebuy.model.pojo.Products
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class CategoryViewModel(var myRepo: IRepository) : ViewModel() {
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
-    }
-    val text: LiveData<String> = _text
-    private var _allProducts = MutableLiveData<ResultState<Products>>()
-    val allProducts: LiveData<ResultState<Products>> get() = _allProducts
-
+    private var _allProducts = MutableStateFlow<ResultState<Products>>(ResultState.Loading)
+    val allProducts get() = _allProducts.asStateFlow()
     fun getAllProduct(category: Long = 0) {
-        _allProducts.postValue(ResultState.Loading)
         viewModelScope.launch(Dispatchers.IO) {
-            val result =
-                if (category == 0L) myRepo.getAllProducts() else myRepo.getProductsByCollectionID(
-                    category
-                )
-            when (result) {
-                is NetworkResponse.FailureResponse ->
-                    _allProducts.postValue(ResultState.Error(result.errorString))
-                is NetworkResponse.SuccessResponse -> {
-                    if (result.data.products.isNullOrEmpty()) {
-                        _allProducts.postValue(ResultState.EmptyResult)
-                    } else
-                        _allProducts.postValue(ResultState.Success(result.data))
-                }
+            _allProducts.emit(ResultState.Loading)
+            val result = async {
+                if (category == 0L) myRepo.getAllProducts()
+                else myRepo.getProductsByCollectionID(category)
+            }
+            sendResponseBack(result.await())
+        }
+    }
 
+    private suspend fun sendResponseBack(result: NetworkResponse<Products>) {
+        when (result) {
+            is NetworkResponse.FailureResponse ->
+                _allProducts.emit(ResultState.Error(result.errorString))
+            is NetworkResponse.SuccessResponse -> {
+                if (result.data.products.isNullOrEmpty()) {
+                    _allProducts.emit(ResultState.EmptyResult)
+                } else
+                    _allProducts.emit(ResultState.Success(result.data))
             }
         }
     }
