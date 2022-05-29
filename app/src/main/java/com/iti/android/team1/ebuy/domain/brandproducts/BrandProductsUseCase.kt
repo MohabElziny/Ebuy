b8ebf79d8eac74c02dc60f79ac878f63669aade8
Mohab El-Ziny <32043983+MohabElziny@users.noreply.github.com>
2022-05-30 00:09:21 +0200

    util function for domain layer

--- app/src/main/java/com/iti/android/team1/ebuy/domain/brandproducts/BrandProductsUseCase.kt
+++ app/src/main/java/com/iti/android/team1/ebuy/domain/brandproducts/BrandProductsUseCase.kt
@@ -1,32 +1,13 @@
 package com.iti.android.team1.ebuy.domain.brandproducts
 
+import com.iti.android.team1.ebuy.domain.productsResponse
 import com.iti.android.team1.ebuy.model.datasource.repository.IRepository
 import com.iti.android.team1.ebuy.model.networkresponse.NetworkResponse
 import com.iti.android.team1.ebuy.model.pojo.Products
 
 class BrandProductsUseCase(private val repository: IRepository) : IBrandProductsUseCase {
     override suspend fun getProductsByCollectionID(collectionID: Long): NetworkResponse<Products> {
-        return productsResponse(repository.getProductsByCollectionID(collectionID))
+        return productsResponse(repository.getProductsByCollectionID(collectionID),
+            repository.getAllFavoritesProducts())
     }
-
-    private suspend fun productsResponse(networkResponse: NetworkResponse<Products>): NetworkResponse<Products> {
-        when (networkResponse) {
-            is NetworkResponse.FailureResponse -> return networkResponse
-            is NetworkResponse.SuccessResponse -> {
-                val productMap = networkResponse.data.products?.associateBy {
-                    it.productID
-                }
-
-                repository.getAllFavoritesProducts().forEach {
-                    productMap?.get(it.productID)?.apply {
-                        isFavorite = true
-                    }
-                }
-
-                return NetworkResponse.SuccessResponse(Products(productMap?.values?.toList()
-                    ?: emptyList()))
-            }
-        }
-    }
-
 }
--- app/src/main/java/com/iti/android/team1/ebuy/domain/category/CategoryProductsUseCase.kt
+++ app/src/main/java/com/iti/android/team1/ebuy/domain/category/CategoryProductsUseCase.kt
@@ -1,5 +1,6 @@
 package com.iti.android.team1.ebuy.domain.category
 
+import com.iti.android.team1.ebuy.domain.productsResponse
 import com.iti.android.team1.ebuy.model.datasource.repository.IRepository
 import com.iti.android.team1.ebuy.model.networkresponse.NetworkResponse
 import com.iti.android.team1.ebuy.model.pojo.Categories
@@ -10,7 +11,7 @@ class CategoryProductsUseCase(
 ) : ICategoryProductsUseCase {
 
     override suspend fun getAllProducts(): NetworkResponse<Products> {
-        return productsResponse(repository.getAllProducts())
+        return productsResponse(repository.getAllProducts(), repository.getAllFavoritesProducts())
     }
 
     override suspend fun getAllCategories(): NetworkResponse<Categories> {
@@ -22,32 +23,12 @@ class CategoryProductsUseCase(
         productType: String,
     ): NetworkResponse<Products> {
         val networkResponse = repository.getAllCategoryProducts(collectionID, productType)
-        return productsResponse(networkResponse)
-    }
-
-    private suspend fun productsResponse(networkResponse: NetworkResponse<Products>): NetworkResponse<Products> {
-        when (networkResponse) {
-            is NetworkResponse.FailureResponse -> return networkResponse
-            is NetworkResponse.SuccessResponse -> {
-                val productMap = networkResponse.data.products?.associateBy {
-                    it.productID
-                }
-
-                repository.getAllFavoritesProducts().forEach {
-                    productMap?.get(it.productID)?.apply {
-                        isFavorite = true
-                    }
-                }
-
-                return NetworkResponse.SuccessResponse(Products(productMap?.values?.toList()
-                    ?: emptyList()))
-            }
-        }
+        return productsResponse(networkResponse, repository.getAllFavoritesProducts())
     }
 
     override suspend fun getProductsByCollectionID(collectionID: Long): NetworkResponse<Products> {
         val networkResponse = repository.getProductsByCollectionID(collectionID)
-        return productsResponse(networkResponse)
+        return productsResponse(networkResponse, repository.getAllFavoritesProducts())
     }
 
 }
