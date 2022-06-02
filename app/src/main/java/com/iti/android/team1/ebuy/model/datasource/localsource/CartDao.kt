597085064600f82e0651c1fc6edf437a0e49c0a5
Mohab El-Ziny <32043983+MohabElziny@users.noreply.github.com>
2022-06-02 18:38:19 +0200

    remove import room.* and import only what I want

--- app/src/main/java/com/iti/android/team1/ebuy/model/datasource/localsource/CartDao.kt
+++ app/src/main/java/com/iti/android/team1/ebuy/model/datasource/localsource/CartDao.kt
@@ -1,6 +1,9 @@
 package com.iti.android.team1.ebuy.model.datasource.localsource
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.iti.android.team1.ebuy.model.pojo.CartItem
 
 @Dao
