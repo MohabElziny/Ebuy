4131f83537021a9b7ebd61e2f8ab2d187621afc8
mina wael <menawael831@gmail.com>
2022-06-03 21:16:39 +0200

    order screen ui

---
+++ app/src/main/java/com/iti/android/team1/ebuy/ui/orders/viewModel/OrdersViewModel.kt
@@ -0,0 +1,8 @@
+package com.iti.android.team1.ebuy.ui.orders.viewModel
+
+import androidx.lifecycle.ViewModel
+import com.iti.android.team1.ebuy.model.datasource.repository.IRepository
+
+class OrdersViewModel(repoInterface:IRepository) : ViewModel() {
+    // TODO: Implement the ViewModel
+}
