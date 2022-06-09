14f8bd0f8888e43520fc01c4245ee9eec2c17d92
Abdulrahman <abdulrahman.ali19@outlook.com>
2022-06-09 14:34:16 +0200

    hot fix

--- app/src/main/java/com/iti/android/team1/ebuy/ui/all_addresses/adapters/AddressAdapter.kt
+++ app/src/main/java/com/iti/android/team1/ebuy/ui/all_addresses/adapters/AddressAdapter.kt
@@ -1,7 +1,6 @@
 package com.iti.android.team1.ebuy.ui.all_addresses.adapters
 
 import android.annotation.SuppressLint
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
