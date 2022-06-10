dac31ca9436c88716621e3ad91a5ba70e826139c
Abdulrahman <abdulrahman.ali19@outlook.com>
2022-06-10 02:49:25 +0200

    payment test

--- app/src/main/java/com/iti/android/team1/ebuy/ui/payment_screen/PaymentFragment.kt
+++ app/src/main/java/com/iti/android/team1/ebuy/ui/payment_screen/PaymentFragment.kt
@@ -1,32 +1,46 @@
 package com.iti.android.team1.ebuy.ui.payment_screen
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.iti.android.team1.ebuy.R
+import androidx.fragment.app.Fragment
+import com.iti.android.team1.ebuy.databinding.FragmentPaymentBinding
+import com.iti.android.team1.ebuy.util.PAYPAL_CLINET_ID
+import com.paypal.checkout.PayPalCheckout
+import com.paypal.checkout.config.CheckoutConfig
+import com.paypal.checkout.config.Environment
+import com.paypal.checkout.config.SettingsConfig
+import com.paypal.checkout.createorder.CurrencyCode
+import com.paypal.checkout.createorder.UserAction
 
 class PaymentFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = PaymentFragment()
-    }
 
     private lateinit var viewModel: PaymentViewModel
-
+    private lateinit var binding: FragmentPaymentBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_payment, container, false)
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentPaymentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(PaymentViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val config = CheckoutConfig(
+            application = requireActivity().application,
+            clientId = PAYPAL_CLINET_ID,
+            environment = Environment.SANDBOX,
+            returnUrl = "BuildConfig.APPLICATION_ID",
+            currencyCode = CurrencyCode.USD,
+            userAction = UserAction.PAY_NOW,
+            settingsConfig = SettingsConfig(
+                loggingEnabled = true
+            )
+        )
+        PayPalCheckout.setConfig(config)
     }
 
 }
---
+++ app/src/main/java/com/iti/android/team1/ebuy/util/config.kt
@@ -0,0 +1,3 @@
+package com.iti.android.team1.ebuy.util
+
+const val PAYPAL_CLINET_ID = "ASDLlGwpxhQ4n2CSc0Qon0Q1RvdXyjdp4-WsmaJF6B98uPzh65H8-hIsbxWvbBNqRKL2IPDinyUto34s"
