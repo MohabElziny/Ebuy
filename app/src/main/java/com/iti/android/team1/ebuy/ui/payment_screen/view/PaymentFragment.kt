67233ba09306c2da9850dacf076f7721662b4c6e
Abdulrahmanali19 <Abdulrahman.ali19@outlook.com>
2022-06-12 22:33:48 +0200

    creating the PaymentViewModelFactory.kt

--- app/src/main/java/com/iti/android/team1/ebuy/ui/payment_screen/view/PaymentFragment.kt
+++ app/src/main/java/com/iti/android/team1/ebuy/ui/payment_screen/view/PaymentFragment.kt
@@ -1,4 +1,4 @@
-package com.iti.android.team1.ebuy.ui.payment_screen
+package com.iti.android.team1.ebuy.ui.payment_screen.view
 
 import android.os.Bundle
 import android.util.Log
@@ -6,7 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.iti.android.team1.ebuy.databinding.FragmentPaymentBinding
+import com.iti.android.team1.ebuy.model.datasource.localsource.LocalSource
+import com.iti.android.team1.ebuy.model.datasource.repository.Repository
+import com.iti.android.team1.ebuy.ui.payment_screen.viewmodel.PaymentViewModel
+import com.iti.android.team1.ebuy.ui.payment_screen.viewmodel.PaymentViewModelFactory
 import com.iti.android.team1.ebuy.util.PAYPAL_CLIENT_ID
 import com.paypal.checkout.PayPalCheckout
 import com.paypal.checkout.approve.OnApprove
@@ -23,14 +28,16 @@ import com.paypal.checkout.order.Amount
 import com.paypal.checkout.order.AppContext
 import com.paypal.checkout.order.Order
 import com.paypal.checkout.order.PurchaseUnit
-import com.paypal.pyplcheckout.BuildConfig
 
 private const val TAG = "PaymentFragment"
 
 class PaymentFragment : Fragment() {
 
-    private lateinit var viewModel: PaymentViewModel
+    private val viewModel: PaymentViewModel by viewModels {
+        PaymentViewModelFactory(Repository(LocalSource(requireContext())))
+    }
     private lateinit var binding: FragmentPaymentBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -78,14 +85,14 @@ class PaymentFragment : Fragment() {
             onApprove =
             OnApprove { approval ->
                 approval.orderActions.capture { captureOrderResult ->
-                    Log.i(TAG, "CaptureOrder: CaptureOrderResult: $captureOrderResult")
+                    ///TODO: ADD order to API
                 }
             },
             onCancel = OnCancel {
-                Log.d(TAG, "OnCancel: Buyer canceled the PayPal experience.")
+                ///TODO: Indicate the user that he has canceled the order
             },
             onError = OnError { errorInfo ->
-                Log.d(TAG, "OnError: $errorInfo")
+                ///TODO: Indicate the user that there's an error the order
             }
 
         )
---
+++ app/src/main/java/com/iti/android/team1/ebuy/ui/payment_screen/viewmodel/PaymentViewModel.kt
@@ -0,0 +1,8 @@
+package com.iti.android.team1.ebuy.ui.payment_screen.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.iti.android.team1.ebuy.model.datasource.repository.IRepository
+
+class PaymentViewModel(private val repo: IRepository) : ViewModel() {
+    // TODO: Implement the ViewModel
+}
