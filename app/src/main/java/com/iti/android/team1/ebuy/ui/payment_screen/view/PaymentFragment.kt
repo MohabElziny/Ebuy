6333af24bb490b1c2f821c90edf5dcb473778510
Abdulrahmanali19 <Abdulrahman.ali19@outlook.com>
2022-06-12 22:34:47 +0200

    creating the PaymentViewModelFactory.kt

--- app/src/main/java/com/iti/android/team1/ebuy/ui/payment_screen/view/PaymentFragment.kt
+++ app/src/main/java/com/iti/android/team1/ebuy/ui/payment_screen/view/PaymentFragment.kt
@@ -1,7 +1,6 @@
 package com.iti.android.team1.ebuy.ui.payment_screen.view
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
