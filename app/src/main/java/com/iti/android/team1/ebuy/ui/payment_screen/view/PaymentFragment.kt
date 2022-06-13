af8c37a1b6b8e2b7fb0a5f7177ee3a74b047d7c0
Abdulrahman <abdulrahman.ali19@outlook.com>
2022-06-13 11:16:13 +0200

    payment CAPTURE

--- app/src/main/java/com/iti/android/team1/ebuy/ui/payment_screen/view/PaymentFragment.kt
+++ app/src/main/java/com/iti/android/team1/ebuy/ui/payment_screen/view/PaymentFragment.kt
@@ -1,6 +1,7 @@
 package com.iti.android.team1.ebuy.ui.payment_screen.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -69,7 +70,7 @@ class PaymentFragment : Fragment() {
             CreateOrder { createOrderActions ->
                 val order =
                     Order(
-                        intent = OrderIntent.AUTHORIZE,
+                        intent = OrderIntent.CAPTURE,
                         appContext = AppContext(userAction = UserAction.PAY_NOW),
                         purchaseUnitList =
                         listOf(
@@ -85,6 +86,7 @@ class PaymentFragment : Fragment() {
             OnApprove { approval ->
                 approval.orderActions.capture { captureOrderResult ->
                     ///TODO: ADD order to API
+                    Log.d(TAG, "OnApprove: $captureOrderResult")
                 }
             },
             onCancel = OnCancel {
@@ -92,6 +94,8 @@ class PaymentFragment : Fragment() {
             },
             onError = OnError { errorInfo ->
                 ///TODO: Indicate the user that there's an error the order
+                Log.d(TAG, "OnApprove: $errorInfo")
+
             }
 
         )
