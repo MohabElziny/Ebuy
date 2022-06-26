36cdb1163046abafff810666516d75195bc2ebf8
Mohab El-Ziny <32043983+MohabElziny@users.noreply.github.com>
2022-06-26 12:43:25 +0200

    mange imports in ProfileFragment.kt

--- app/src/main/java/com/iti/android/team1/ebuy/ui/profile_screen/view/ProfileFragment.kt
+++ app/src/main/java/com/iti/android/team1/ebuy/ui/profile_screen/view/ProfileFragment.kt
@@ -2,7 +2,12 @@ package com.iti.android.team1.ebuy.ui.profile_screen.view
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
